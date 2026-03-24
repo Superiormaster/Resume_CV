3af6d2d771a274d94785f27b2694e152e7ef06a6
Superiormaster <ejeziepaschal@gmail.com>
2026-03-24 14:56:30 +0100

    good

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-import cloudinary
 
 load_dotenv()
 
@@ -12,22 +11,5 @@ class BaseConfig:
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', os.path.join(BASE_DIR, 'uploads'))
     RESUME_FOLDER = os.environ.get('RESUME_FOLDER', os.path.join(BASE_DIR, 'resumes'))
 
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
-
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = MAIL_USERNAME
-    MAX_CONTENT_LENGTH = 10 * 1024 * 1024
-
 class Config(BaseConfig):
     DEBUG = True
-
-cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
-    secure=True
-)
